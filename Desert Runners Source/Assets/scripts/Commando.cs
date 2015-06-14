8665ef33e31d118f24c277075468fc5858e40f5f
atoMerz <hmorteza@gmail.com>
2015-06-14 15:40:12 +0430

    - Fixed code formatting problem.  - Activated 3rd commando.

--- Desert Runners Source/Assets/scripts/Commando.cs
+++ Desert Runners Source/Assets/scripts/Commando.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class Commando : MonoBehaviour
 {
 
-	void OnTriggerEnter2D(Collider2D col)
-	{
-		GameObject.Find("GameManager").GetComponent<GameManagerScript>().gameOver();
-	}
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        GameObject.Find("GameManager").GetComponent<GameManagerScript>().gameOver();
+    }
 	
 }
