b2bf480caee61b6e3b95fce841074736c0ce2302
atoMerz <hmorteza@gmail.com>
2015-07-03 15:32:58 +0430

    - Refactored background movement codes in Main Menu to be used in Game Scene as well.

--- Desert Runners Source/Assets/scripts/InfiniteBackgroundCreator.cs
+++ Desert Runners Source/Assets/scripts/InfiniteBackgroundCreator.cs
@@ -4,15 +4,30 @@ using System.Collections;
 public class InfiniteBackgroundCreator : MonoBehaviour
 {
     public float speed;
+    public Vector3 initialPos;
+    public float patternLen;
+    public GameObject[] backgroundItems;
 	
+    void Start()
+    {
+        for (int i=0; i<backgroundItems.Length; ++i)
+            backgroundItems [i].transform.position = new Vector3(initialPos.x + i * patternLen, initialPos.y, initialPos.z);
+    }    
+    
     void Update()
     {
-        this.transform.Translate(-speed * Time.deltaTime, 0, 0);
-        if (this.transform.position.x < -2048)
+        for (int i=0; i<backgroundItems.Length; ++i)
+            moveBackgroundItem(backgroundItems [i]);
+    }
+    
+    private void moveBackgroundItem(GameObject gameObj)
+    {
+        gameObj.transform.Translate(-speed * Time.deltaTime, 0, 0);
+        if (gameObj.transform.position.x < -patternLen)
         {
-            Vector3 currentPos = this.transform.position;
-            this.transform.position = new Vector3(currentPos.x + 4096, currentPos.y, currentPos.z);
+            Vector3 currentPos = gameObj.transform.position;
+            print("moving to: " + currentPos.x + backgroundItems.Length * patternLen);
+            gameObj.transform.position = new Vector3(currentPos.x + backgroundItems.Length * patternLen, initialPos.y, initialPos.z);
         }
-            
     }
 }
---
+++ Desert Runners Source/Assets/scripts/MovingObject.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingObject : MonoBehaviour
+{
+    
+    public float speed;
+    public Vector3 initialPos;
+    public float leftHorizontalBound;
+    
+    void Start()
+    {
+        this.transform.position = new Vector3(initialPos.x, initialPos.y, initialPos.z);
+    }    
+    
+    void Update()
+    {
+        this.transform.Translate(-speed * Time.deltaTime, 0, 0);
+        if (this.transform.position.x < leftHorizontalBound)
+        {
+            Vector3 currentPos = this.transform.position;
+            this.transform.position = new Vector3(initialPos.x, initialPos.y, initialPos.z);
+        }
+    }
+    
+}
