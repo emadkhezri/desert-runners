8642450f550a3758482fe123cbf113c328544a6b
Emad <emad.khezri@gmail.com>
2015-07-04 17:17:41 +0430

    -

---
+++ Desert Runners Source/Assets/scripts/SettingsFile.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class SettingsFile
+{
+    private string fileName;
+    
+    public SettingsFile(string fileName)
+    {
+        this.fileName = fileName;
+    }
+    
+    public bool RTL
+    {
+        get;
+        set;
+    }
+    
+    /**
+    * @throws Exception
+    */
+    public void save()
+    {
+        File.WriteAllText(fileName, RTL.ToString());
+    }
+    
+    /**
+    * @throws Exception
+    */
+    public void load()
+    {
+        RTL = bool.Parse(File.ReadAllText(fileName));
+    }
+}
